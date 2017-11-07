803e5636d4d575069cd7a25550161f83ce4a0947
Rajasekar T <kt.rajasekar@gmail.com>
2017-11-07 15:45:47 +0530

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,50 @@ import logo1 from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo1} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
+// Increment 
+class Bt extends Component {
+  state ={ counters :10 };
+  handleClick = () =>{
+      this.setState({
+          counters:this.state.counters+1
+      })
+  };
+  
+      render(){
+          return(
+              <button onClick={this.handleClick}>
+                  {this.state.counters}
+              </button>   
+          );
+      }
+    }
+    class Header extends Component {
+      render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo1} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        );
+      }
+    }
+
+  class App extends Component{
+    render(){
+      return(
+        <div>
+        <Header />
+        <Bt />
+        </div>
+      );
+    }
+  }
 
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,37 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-
-class Bt extends Component {
-state ={ counters :10 };
-
-handleClick = () =>{
-    this.setState({
-        counters:this.state.counters+1;
-    })
-};
-
-    render(){
-        return(
-            <button onClick={this.handleClick}>
-                {this.state.counters}
-            </button>   
-        );
-    }
-  }
-
-
 let models = (<div className="container"> 
-{/* <App /> */}
-<Bt />
+    <App />
  </div>
 );
-
-
 
 
 ReactDOM.render(models, document.getElementById('root'));
